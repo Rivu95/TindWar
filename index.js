704d0e7a8a23ebb4de5cadba7c17cefaead34317
Niladri Ghosh <43736801+Rivu95@users.noreply.github.com>
2021-06-08 11:29:34 +0530

    index file

---
+++ index.js
@@ -0,0 +1,83 @@
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const fs = require("fs");
+require('dotenv').config();
+const cron = require("node-cron");
+
+const client = new Client({ ws: { intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] } });
+client.cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+	console.log('I am Ready');
+	client.user.setActivity("+help", { type: "LISTENING" });
+});
+
+//------------------------------- Interaction Handler -------------------------------//
+const Interaction = require("./interactions/INTERACTION_CREATE");
+client.ws.on('INTERACTION_CREATE', async interaction => {
+	Interaction.run(client, interaction);
+})
+
+client.interactions = new Discord.Collection();
+fs.readdir("./slash Commands", (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		if (!file.endsWith(".js")) return;
+		let props = require(`./slash Commands/${file}`);
+		let interactionName = file.split(".")[0];
+		console.log(`Attempting to load slashes ${interactionName}`);
+		client.interactions.set(interactionName, props);
+	});
+});
+
+//------------------------------- Event Handler -------------------------------//
+fs.readdir("./events/", (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		const eventFunction = require(`./events/${file}`);
+		if (eventFunction.disabled) return;
+
+		const event = eventFunction.event || file.split(".")[0];
+		const emitter =
+			(typeof eventFunction.emitter === "string"
+				? client[eventFunction.emitter]
+				: eventFunction.emitter) || client;
+		const once = eventFunction.once;
+
+		try {
+			emitter[once ? "once" : "on"](event, (...args) =>
+				eventFunction.run(client, ...args)
+			);
+		} catch (error) {
+			console.error(error.stack);
+		}
+	});
+});
+
+//------------------------------- Command Handler -------------------------------//
+client.commands = new Discord.Collection();
+let commandFolders = fs
+	.readdirSync("./commands");
+
+for (const folder of commandFolders) {
+	let commandFiles = fs
+		.readdirSync(`./commands/${folder}`)
+		.filter(file => file.endsWith(".js"));
+
+	for (const file of commandFiles) {
+		let command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
+}
+
+//------------------------------- Error Logging -------------------------------//
+process.on("unhandledRejection", error => {
+	console.error("Unhandled promise rejection:", error);
+});
+
+//------------------------------- Logging In -------------------------------//
+client.login(process.env.bot_token);
+
+
+
+//------------------------------- Cron Job Handler -------------------------------//
