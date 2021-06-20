f9b02ea50e4cc330e3c8ec90e38403a758bf8687
Suvajit <suvajit.me@gmail.com>
2021-06-20 16:55:43 +0530

    create tables

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,15 @@
 require("dotenv").config();
-
 const { Pool } = require("pg");
+const pool = new Pool({ connectionString: process.env.DB_URL });
 
-const connectionString = process.env.DB_URL;
-const pool = new Pool({ connectionString });
-
-pool.on("error", (err, client) => {
-	console.error("Unexpected error on idle client", err);
+pool.on("error", (error, client) => {
+	console.error("Unexpected error on idle client", error);
 	process.exit(-1);
 });
 
 const run = async () => {
 	await pool.query(
-		`
-        CREATE TABLE server_clan_registry (
+		`CREATE TABLE server_clan_registry (
             server_id text NOT NULL,
             team_name text NULL,
             clan_tag text NULL,
@@ -25,8 +21,41 @@ const run = async () => {
             last_played_with text NULL,
             channel_id text NULL,
             CONSTRAINT server_clan_registry_pk PRIMARY KEY (server_id)
-        )
-        `
+        )`
+	);
+
+	await pool.query(
+		`CREATE TABLE war_match (
+			clan_tag text NOT NULL,
+			search_time text NULL,
+			entry_time timestamp(0) NULL,
+			search_end_time timestamp(0) NULL,
+			server_id text NOT NULL,
+			CONSTRAINT war_match_pk PRIMARY KEY (server_id)
+		)`
+	);
+
+	await pool.query(
+		`CREATE TABLE war_history (
+			server_1 text NULL,
+			server_2 text NULL,
+			matched_at timestamp(0) NULL,
+			winner text NULL,
+			clan_1 text NULL,
+			clan_2 text NULL,
+			match_id serial NOT NULL,
+			CONSTRAINT war_history_pk PRIMARY KEY (match_id)
+		)`
+	);
+
+	await pool.query(
+		`CREATE TABLE stats (
+			bot_id text NOT NULL,
+			server_count int4 NULL,
+			war_matched int4 NULL,
+			commands_per_week int4 NULL,
+			CONSTRAINT stats_pk PRIMARY KEY (bot_id)
+		)`
 	);
 };
 
