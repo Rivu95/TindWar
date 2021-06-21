9eb9595d6a8732f36541e4d23eec169d4da7e605
Niladri Ghosh <43736801+Rivu95@users.noreply.github.com>
2021-06-21 20:48:14 +0530

    status change after war seach time completes

--- src/utils/searchEndScript.js
+++ src/utils/searchEndScript.js
@@ -4,14 +4,20 @@ const serverClanDB = require("../database/serverClanData");
 
 module.exports.run = async (client) => {
 	const wait_list = await DB.deleteClanByTime();
-	const server = await serverClanDB.getServer(wait_list.server_id);
-	const channel = client.channels.cache.get(server.channel_id);
 
-	const embed = new Discord.MessageEmbed()
-		.setColor("#ff0000")
-		.setTitle("Sorry! No Match Found")
-		.setDescription("Use the find-war slash command again to find a match!")
-		.setTimestamp();
+	if (wait_list) {
+		// Updating presence
+		client.user.setActivity(`slash commands`, { type: "LISTENING" });
 
-	return channel.send(embed);
+		const server = await serverClanDB.getServer(wait_list?.server_id);
+		const channel = client.channels.cache.get(server.channel_id);
+
+		const embed = new Discord.MessageEmbed()
+			.setColor("#ff0000")
+			.setTitle("Sorry! No Match Found")
+			.setDescription("Use the find-war slash command again to find a match!")
+			.setTimestamp();
+
+		return channel.send(embed);
+	}
 };
